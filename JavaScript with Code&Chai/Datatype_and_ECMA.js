46726e354e0b4fb9ab9468faa42841be599694c8
sumitiitj493-boop <sumitiitj493@gmail.com>
2025-10-17 11:19:50 +0530

    updated

---
+++ JavaScript with Code&Chai/Datatype_and_ECMA.js
@@ -0,0 +1,29 @@
+/* 
+ //alert(3+3);           this will show error because alert is not defined
+//                       in node js while it works in browser with popup
+//Remember: JavaScript engine is embedded in browsers, while Node.js provides a different runtime en-vironment
+ console.log(3+3); //this will work in both browser and node js
+
+  */
+ "use strict"; // it is just a directive to use strict mode of js.no need to write in every file but it is a good practice
+ let naam ="Simit";
+ let age = 21;
+ let issmart = true;
+ let height; //undefined
+ let weight = null; //null
+ let id=Symbol("id"); //symbol
+ let bigNumber=123456789012345678904831n; //bigint
+
+console.log(typeof age);
+console.log(typeof naam);
+console.log(typeof issmart);
+console.log(typeof height);
+console.log(typeof weight);
+console.log(typeof id);
+console.log(typeof bigNumber);
+
+console.table([naam, age, issmart, height, weight, id, bigNumber]);
+height=1.75;
+console.log("Updated height:", height);
+console.table([naam, age, issmart, height, weight, id, bigNumber]);
+

2b317af82a393d7eb68ab192201d0f63f84d38d7
sumitiitj493-boop <sumitiitj493@gmail.com>
2025-10-17 08:07:14 +0530

    update

---
+++ JavaScript with Code&Chai/Var_vs_Let_.js
@@ -0,0 +1,10 @@
+const account_id = "12345";
+let user_name = "JohnDoe";
+let email = "sumkr493@gmail.com";
+let city = "New York";
+let country = "USA";
+country = "India";
+city = "Mumbai";
+//account_id = "54321"; // This will cause an error because account_id is declared with const
+console.log(account_id);
+console.table([account_id,user_name, email, city, country]);
